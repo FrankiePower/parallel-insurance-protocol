e159da174cab43c082d4883929f5aec617fb60cf
SuperFranky <franklin.power21@gmail.com>
2025-10-23 01:12:28 +0100

    packages and configs

--- hardhat.config.js
+++ hardhat.config.js
@@ -9,8 +9,11 @@ module.exports = {
   solidity: {
     compilers: [
       {
-        version: "0.8.19",
-        settings: { optimizer: { enabled: true, runs: 200 } },
+        version: "0.8.20",
+        settings: {
+          optimizer: { enabled: true, runs: 200 },
+          viaIR: true
+        },
       },
       {
         version: "0.4.18",
